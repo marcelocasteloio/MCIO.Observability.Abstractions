46760d73c5f5785ec8fc34ee7be8808d2c1aa400
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-02-05 00:57:33 -0300

    refact: change do overload methods are adjacents

--- src/Observability.Abstractions/ITraceManager.cs
+++ src/Observability.Abstractions/ITraceManager.cs
@@ -50,27 +50,25 @@ namespace MCIO.Observability.Abstractions
         Task<TOutput> StartClientActivityAsync<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
         Task<TOutput> StartClientActivityAsync<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
 
+        void StartConsumerActivity(string traceName, ExecutionInfo executionInfo, Action<Activity, ExecutionInfo> handler);
+        void StartConsumerActivity<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Action<Activity, ExecutionInfo, TInput> handler);
+        TOutput StartConsumerActivity<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, TOutput> handler);
+        TOutput StartConsumerActivity<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, TOutput> handler);
+
+        Task StartConsumerActivityAsync(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task> handler, CancellationToken cancellationToken);
+        Task StartConsumerActivityAsync<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task> handler, CancellationToken cancellationToken);
+        Task<TOutput> StartConsumerActivityAsync<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
+        Task<TOutput> StartConsumerActivityAsync<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
 
         void StartProducerActivity(string traceName, ExecutionInfo executionInfo, Action<Activity, ExecutionInfo> handler);
         void StartProducerActivity<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Action<Activity, ExecutionInfo, TInput> handler);
         TOutput StartProducerActivity<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, TOutput> handler);
         TOutput StartProducerActivity<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, TOutput> handler);
 
-        Task StartConsumerActivityAsync(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task> handler, CancellationToken cancellationToken);
         Task StartProducerActivityAsync<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task> handler, CancellationToken cancellationToken);
         Task<TOutput> StartProducerActivityAsync<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
         Task<TOutput> StartProducerActivityAsync<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
-
-
-        void StartConsumerActivity(string traceName, ExecutionInfo executionInfo, Action<Activity, ExecutionInfo> handler);
-        void StartConsumerActivity<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Action<Activity, ExecutionInfo, TInput> handler);
-        TOutput StartConsumerActivity<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, TOutput> handler);
-        TOutput StartConsumerActivity<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, TOutput> handler);
-
         Task StartProducerActivityAsync(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task> handler, CancellationToken cancellationToken);
-        Task StartConsumerActivityAsync<TInput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task> handler, CancellationToken cancellationToken);
-        Task<TOutput> StartConsumerActivityAsync<TOutput>(string traceName, ExecutionInfo executionInfo, Func<Activity, ExecutionInfo, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
-        Task<TOutput> StartConsumerActivityAsync<TInput, TOutput>(string traceName, ExecutionInfo executionInfo, TInput input, Func<Activity, ExecutionInfo, TInput, CancellationToken, Task<TOutput>> handler, CancellationToken cancellationToken);
     }
 
 }
