afdf4be1cd1171858e02b393197b0ed55236b09d
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-02-10 19:18:12 -0300

    refact: change record histogram overload

--- src/Observability.Abstractions/IMetricsManager.cs
+++ src/Observability.Abstractions/IMetricsManager.cs
@@ -14,7 +14,7 @@ namespace MCIO.Observability.Abstractions
 
         void CreateHistogram<T>(string name, string unit = null, string description = null) where T : struct;
         void RecordHistogram<T>(string name, T value) where T : struct;
-        void RecordHistogram<T>(string name, T value, KeyValuePair<string, object> tag) where T : struct;
+        void RecordHistogram<T>(string name, T value, KeyValuePair<string, object>[] tags) where T : struct;
         IEnumerable<Histogram> GetHistogramCollection();
 
         void CreateObservableGauge<T>(string name, Func<IEnumerable<Measurement<T>>> observeValues, string unit = null, string description = null) where T : struct;
